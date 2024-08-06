ce329df07119a923432fa792a25e0ddd914118e1
yanovitchsky <yannakoun@gmail.com>
2024-08-06 15:08:13 +0000

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,91 @@
+/* global OT API_KEY TOKEN SESSION_ID SAMPLE_SERVER_BASE_URL */
+
+let apiKey;
+let sessionId;
+let token;
+let myPublisher;
+
+function handleError(error) {
+  if (error) {
+    console.error(error);
+  }
+}
+
+function initializeSession() {
+  const session = OT.initSession(apiKey, sessionId);
+
+  // Subscribe to a newly created stream
+  session.on('streamCreated', (event) => {
+    const subscriberOptions = {
+      insertMode: 'append',
+      width: '100%',
+      height: '100%',
+    };
+    session.subscribe(
+      event.stream,
+      'subscriber',
+      subscriberOptions,
+      handleError
+    );
+  });
+
+  session.on('sessionDisconnected', (event) => {
+    console.log('You were disconnected from the session.', event.reason);
+  });
+
+  // initialize the publisher
+  const publisherOptions = {
+    insertMode: 'append',
+    width: '100%',
+    height: '100%',
+    showControls: false,
+  };
+  const publisher = OT.initPublisher(
+    'publisher',
+    publisherOptions,
+    handleError
+  );
+  myPublisher = publisher;
+
+  // Connect to the session
+  session.connect(token, (error) => {
+    if (error) {
+      handleError(error);
+    } else {
+      // If the connection is successful, publish the publisher to the session
+      session.publish(publisher, handleError);
+    }
+  });
+}
+
+// See the config.js file.
+if (API_KEY && TOKEN && SESSION_ID) {
+  apiKey = API_KEY;
+  sessionId = SESSION_ID;
+  token = TOKEN;
+  initializeSession();
+} else if (SAMPLE_SERVER_BASE_URL) {
+  // Make a GET request to get the OpenTok API key, session ID, and token from the server
+  fetch(SAMPLE_SERVER_BASE_URL + '/session')
+    .then((response) => response.json())
+    .then((json) => {
+      apiKey = json.apiKey;
+      sessionId = json.sessionId;
+      token = json.token;
+      // Initialize an OpenTok Session object
+      initializeSession();
+    })
+    .catch((error) => {
+      handleError(error);
+      alert(
+        'Failed to get opentok sessionId and token. Make sure you have updated the config.js file.'
+      );
+    });
+}
+
+document.getElementById('toggleVideo').addEventListener('click', () => {
+  if (myPublisher) {
+    const videoEnabled = myPublisher.stream.hasVideo;
+    myPublisher.publishVideo(!videoEnabled);
+  }
+});
